13c02aca38ff317a65dd28823cdb8914d1375b6d
ZunigaTomas <zuniga.tomas99@gmail.com>
2024-10-07 11:03:24 -0300

    Primer commit

---
+++ components/ShareModal.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState } from 'react';
+
+const ShareModal = ({ roomId, collaborators, creatorId, currentUserType }: ShareDocumentDialogProps) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>ShareModal</div>
+  )
+}
+
+export default ShareModal;
---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b65e0db6546e73cbcc94b9c1c6f1f012@o4508055166320640.ingest.us.sentry.io/4508055286972416",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
